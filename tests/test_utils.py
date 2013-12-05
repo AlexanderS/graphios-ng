8aab9a8648e2f40276d9b9b81e4ec7a9e18caa0d
Alexander Sulfrian <alexander@sulfrian.net>
2013-12-05 09:12:35 +0100

    tests: add tests for with_log

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+import unittest
+from graphios_ng import utils
+
+
+class UtilsTest(unittest.TestCase):
+    def test_with_log(self):
+        @utils.with_log
+        def testing(log=None):
+            self.assertIsNotNone(log)
+            self.assertEquals(str(type(log)), "<class 'logging.Logger'>")
+        testing()
+
+    def test_with_log_overwrite(self):
+        @utils.with_log
+        def testing(log=None):
+            self.assertEquals(log, 'Should not change')
+        testing(log = 'Should not change')
