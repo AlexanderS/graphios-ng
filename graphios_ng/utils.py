58526b46ad5a6a19b08cf9592a48cd60b443a7ed
Alexander Sulfrian <alexander@sulfrian.net>
2013-12-09 20:34:59 +0100

    utils: add custom Formatter
    Add a custom Formatter class, that allows !q and !f conversions to
    quote URLs and create valid file names from input data.

--- graphios_ng/utils.py
+++ graphios_ng/utils.py
@@ -1,5 +1,13 @@
 from functools import wraps
 import logging
+import re
+import string
+import unicodedata
+
+try:
+    from urllib import quote_plus        # pylint: disable=E0611
+except ImportError:
+    from urllib.parse import quote_plus  # pylint: disable=E0611,F0401
 
 
 def with_log(func):
@@ -10,3 +18,30 @@ def with_log(func):
             kwds['log'] = logger
         return func(*args, **kwds)
     return wrapper
+
+
+def get_valid_filename(s):
+    s = s.strip().replace(' ', '_')
+    return re.sub(r'(?u)[^-\w.]', '', s)
+
+
+def slugify(value):
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = value.decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value).strip().lower()
+    return re.sub(r'[-\s]+', '-', value)
+
+
+class Formatter(string.Formatter):
+    def __init__(self):
+        pass
+
+    def convert_field(self, value, conversion):
+        if conversion is None:
+            return value
+        elif conversion == 'q':
+            return quote_plus(value)
+        elif conversion == 'f':
+            return get_valid_filename(value)
+        return string.Formatter.convert_field(self, value, conversion)
+formatter = Formatter()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 import unittest
 from graphios_ng import utils
 
@@ -15,3 +19,15 @@ class WithLogTest(unittest.TestCase):
         def testing(log=None):
             self.assertEquals(log, 'Should not change')
         testing(log = 'Should not change')
+
+
+class FormatterTest(unittest.TestCase):
+    def test_querystring(self):
+        self.assertEquals(
+            utils.formatter.format('{query!q}', query='string_of_characters_like_these:$#@=?%^Q^$'),
+            'string_of_characters_like_these%3A%24%23%40%3D%3F%25%5EQ%5E%24')
+
+    def test_filename(self):
+        self.assertEquals(
+            utils.formatter.format('{file!f}', file='foobar / test äöüß blub\'s'),
+            'foobar__test_äöüß_blubs')
