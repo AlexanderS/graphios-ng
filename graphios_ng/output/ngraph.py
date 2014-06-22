062675a80b29b6c2b9c9ae1cc3a39c5b0da861fc
Alexander Sulfrian <alexander@sulfrian.net>
2014-06-22 17:21:47 +0200

    output/ngraph: implement dummy output
    Implement a simple dummy output for ngraph style lines with separate
    host and service templates. It currently only builds the lines but does
    no file output.

--- graphios_ng/output/ngraph.py
+++ graphios_ng/output/ngraph.py
@@ -1,5 +1,36 @@
 from graphios_ng.output import Output
+from graphios_ng.utils import Template
+from graphios_ng.configurable import ConfigItem
+
+DEFAULT_HOST_PERFDATA_TEMPLATE = (
+    '${host}\t${output}\t${perfdata}\t${time}\n'
+)
+
+DEFAULT_SERVICE_PERFDATA_TEMPLATE = (
+    '${host}\t${service}\t${output}\t${perfdata}\t${time}\n'
+)
 
 
 class NgraphOutput(Output):
-    pass
+    config_items = [ConfigItem('host_perfdata_template',
+                               default=DEFAULT_HOST_PERFDATA_TEMPLATE),
+                    ConfigItem('service_perfdata_template',
+                               default=DEFAULT_SERVICE_PERFDATA_TEMPLATE)]
+
+    def _build_host_output(self, data):
+        template = Template(self.config['host_perfdata_template'])
+        return template.safe_substitute(**data)
+
+    def _build_service_output(self, data):
+        template = Template(self.config['service_perfdata_template'])
+        return template.safe_substitute(**data)
+
+    def output(self, data):
+        line = None
+        if data['type'] == 'host':
+            line = self._build_host_output(data)
+        elif data['type'] == 'service':
+            line = self._build_service_output(data)
+
+        if line is None:
+            raise ValueError('Invalid output data: %s' % data)
