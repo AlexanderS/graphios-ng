5c2d890a69fffe6fc2b471ca01301922628d589b
Alexander Sulfrian <alexander@sulfrian.net>
2014-06-22 17:29:26 +0200

    parser/ngraph: implement ngraph parser
    Simple parses every line in the files as ngraph expression (tab
    delimited values). Only the basic ngraph values are parsed and
    additional data at end of file is ignored.

--- graphios_ng/parser/ngraph.py
+++ graphios_ng/parser/ngraph.py
@@ -6,10 +6,32 @@ class NgraphParser(FileParser):
     def __init__(self, config, handler):
         super(NgraphParser, self).__init__(config, handler)
 
+    def _parse_values(self, line, count):
+        values = line.split('\t')
+        if len(values) < count:
+            raise ValueError('Invalid format of input line: %s' % line)
+
+        return values
+
     @with_log
     def _parse_host_perfline(self, line, log=None):
-        pass
+        elem = super(NgraphParser, self)._parse_host_perfline(line)
+
+        values = self._parse_values(line, 4)
+        elem['host'] = values[0]
+        elem['output'] = values[1]
+        elem['perfdata'] = values[2]
+        elem['time'] = values[3]
+        return elem
 
     @with_log
     def _parse_service_perfline(self, line, log=None):
-        pass
+        elem = super(NgraphParser, self)._parse_service_perfline(line)
+
+        values = self._parse_values(line, 5)
+        elem['host'] = values[0]
+        elem['service'] = values[1]
+        elem['output'] = values[2]
+        elem['perfdata'] = values[3]
+        elem['time'] = values[4]
+        return elem
